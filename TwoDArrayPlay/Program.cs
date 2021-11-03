d2e6bb86dd2eecd8e12c5d88f72c0f17acbb1836
John-MacAulay <88356693+John-MacAulay@users.noreply.github.com>
2021-11-03 11:50:23 +1100

    just the cs

---
+++ TwoDArrayPlay/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace TwoDArrayPlay
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string [,] twoDArray = new string[3,3];
+            twoDArray[0,0] = ".";
+            twoDArray[1,1] = ".";
+             
+        }
+    }
+}
